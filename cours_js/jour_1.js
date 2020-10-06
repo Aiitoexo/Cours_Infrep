2727730270856c4049da070afceb9e09a371cbb6
Aiitoexo <mack.lachaud@gmail.com>
2020-10-06 12:22:14 +0200

    Exercices 2, 3 ,4

--- cours_js/jour_1.js
+++ cours_js/jour_1.js
@@ -5,14 +5,70 @@
 // "bon" + "jour";
 //
 // console.log("Bonjour");
+//
+// Exerccice 1
+//
+// let a = 3;
+// let b = 4;
+// let c = "Le résultat vaut : ";
+//
+// let d = a + b;
+//
+// console.log(c + d);
+//
+// console.log("Le résultat vaut : " + (3 + 4));
+//
+// //  Déclarer une Variable
+//
+// var oldSchool = 12; // "vieux JS"
+// let newSchool = 12; // Lire "let x be 12"
+//
+// Affecter une valeur a une variable existante
+//
+// x = 10;
+// console.log("X vaut " + x) // = console.log("X vaut" + 10)
+// x = 30;
+//
+// Exercice 2 : Calculer 3 + 3, puis calculer 4 + 2 et
+// enfin multiplier le résultat des deux additions
+// puis afficher le résultat
+//
+// let a = 3 + 3;
+// let b = 4 + 2;
+// let c = a * b;
+//
+// console.log(c); // || console.log(a * b);
+//
+// Exercice 3 : Creer une variable, et lui donner une valeur numerique.
+// Ajouter 1 a ce nombre, et stocker le resultat dans la meme variable
 
-let a = 3;
-let b = 4;
-let c = "Le résultat vaut : ";
+// let a;
+// a = 2
+// a = a + 1
+//
+// console.log(a)
 
-let d = a + b;
+// Exerice 4 : Créer un programme qui permute la valeur
+// de deux variables a et b.
+//     let a = 5;
+// let b = 3;
+// A la fin, a doit avoir la valeur 3 et
+// b doit avoir la valeur 5
+// (Plusieurs solutions possibles !)
 
-console.log(c + d);
+let a = 5;
+let b = 3;
+let c = a;
 
-console.log("Le résultat vaut : " + (3+4));
+a = b;
+b = c;
 
+console.log(a, b);
+
+let d = 5;
+let e = 3;
+
+d = 3;
+e = 5;
+
+console.log(d, e);
