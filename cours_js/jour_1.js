1083f27e41e3d98d9273f3ac0ba8f7710aead8d1
Aiitoexo <mack.lachaud@gmail.com>
2020-10-06 10:47:21 +0200

    Exercices 1

---
+++ cours_js/jour_1.js
@@ -0,0 +1,18 @@
+// 1 + 1;
+//
+// 2 + 1;
+//
+// "bon" + "jour";
+//
+// console.log("Bonjour");
+
+let a = 3;
+let b = 4;
+let c = "Le résultat vaut : ";
+
+let d = a + b;
+
+console.log(c + d);
+
+console.log("Le résultat vaut : " + (3+4));
+
