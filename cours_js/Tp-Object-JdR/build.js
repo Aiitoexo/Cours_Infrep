387a9a724fced387ddd47d9fa3c4ff6a2a242f9a
Aiitoexo <mack.lachaud@gmail.com>
2020-10-21 09:11:06 +0200

    Debut APP JDR et Creation de Build

---
+++ cours_js/Tp-Object-JdR/build.js
@@ -0,0 +1,180 @@
+let warrior = {
+    weapon: null,
+    shield: null,
+    armor: null,
+    stamina: 200,
+    strength: 8,
+    agility: 2,
+    intellect: 1,
+    critical: function (){
+        return 10 * warrior.agility
+    },
+    dodge: function () {
+        // calcul taux d'esquive  suivant agility perso
+    },
+    parade: function () {
+        // calcul taux de parade suivant arme ou shield equiper
+    },
+    spell: {
+        // x Spell + stats +  function random spell
+    },
+    attack: function (target) {
+        // spell calcul de degat
+    },
+    defense: function () {
+        // random parade dodge + calcul de DEF
+    },
+    equipWeapon: function (weapon) {
+        this.weapon = weapon
+        // arme ou shield equiper
+    },
+    equipArmor: function (armor) {
+        this.armor = armor
+        // armure equiper
+    }
+}
+
+let assassin = {
+    weapon: null,
+    shield: null,
+    armor: null,
+    stamina: 120,
+    strength: 6,
+    agility: 9,
+    intellect: 3,
+    critical: function (){
+        return 10 * assassin.agility
+    },
+    dodge: function () {
+        // calcul taux d'esquive  suivant agility perso
+    },
+    parade: function () {
+        // calcul taux de parade suivant arme ou shield equiper
+    },
+    spell: {
+        // x Spell + stats +  function random spell
+    },
+    attack: function (target) {
+        // spell calcul de degat
+    },
+    defense: function () {
+        // random parade dodge + calcul de DEF
+    },
+    equipWeapon: function (weapon) {
+        this.weapon = weapon
+        // arme ou shield equiper
+    },
+    equipArmor: function (armor) {
+        this.armor = armor
+        // armure equiper
+    }
+}
+
+let mage = {
+    weapon: null,
+    shield: null,
+    armor: null,
+    stamina: 100,
+    strength: 2,
+    agility: 6,
+    intellect: 8,
+    critical: function (){
+        return 10 * mage.agility
+    },
+    dodge: function () {
+        // calcul taux d'esquive  suivant agility perso
+    },
+    parade: function () {
+        // calcul taux de parade suivant arme ou shield equiper
+    },
+    spell: {
+        // x Spell + stats +  function random spell
+    },
+    attack: function (target) {
+        // spell calcul de degat
+    },
+    defense: function () {
+        // random parade dodge + calcul de DEF
+    },
+    equipWeapon: function (weapon) {
+        this.weapon = weapon
+        // arme ou shield equiper
+    },
+    equipArmor: function (armor) {
+        this.armor = armor
+        // armure equiper
+    }
+}
+
+let warlock = {
+    weapon: null,
+    shield: null,
+    armor: null,
+    stamina: 100,
+    strength: 2,
+    agility: 6,
+    intellect: 8,
+    critical: function (){
+        return 10 * warlock.agility
+    },
+    dodge: function () {
+        // calcul taux d'esquive  suivant agility perso
+    },
+    parade: function () {
+        // calcul taux de parade suivant arme ou shield equiper
+    },
+    spell: {
+        // x Spell + stats +  function random spell
+    },
+    attack: function (target) {
+        // spell calcul de degat
+    },
+    defense: function () {
+        // random parade dodge + calcul de DEF
+    },
+    equipWeapon: function (weapon) {
+        this.weapon = weapon
+        // arme ou shield equiper
+    },
+    equipArmor: function (armor) {
+        this.armor = armor
+        // armure equiper
+    }
+}
+
+let hunter = {
+    weapon: null,
+    shield: null,
+    armor: null,
+    stamina: 150,
+    strength: 7,
+    agility: 7,
+    intellect: 2,
+    critical: function () {
+        return 10 * hunter.agility
+    },
+    dodge: function () {
+        // calcul taux d'esquive  suivant agility perso
+    },
+    parade: function () {
+        // calcul taux de parade suivant arme ou shield equiper
+    },
+    spell: {
+        // x Spell + stats +  function random spell
+    },
+    attack: function (target) {
+        // spell calcul de degat
+    },
+    defense: function () {
+        // random parade dodge + calcul de DEF
+    },
+    equipWeapon: function (weapon) {
+        this.weapon = weapon
+        // arme ou shield equiper
+    },
+    equipArmor: function (armor) {
+        this.armor = armor
+        // armure equiper
+    }
+}
+
